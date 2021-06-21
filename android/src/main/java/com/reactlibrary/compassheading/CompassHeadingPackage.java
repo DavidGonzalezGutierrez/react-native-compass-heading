f2cdb33c297a1f749528f64c36169bb4edf2976d
DavidGonzalezGutierrez <benaventebiker_007@hotmail.com>
2021-06-21 10:23:44 +0200

    Update CompassHeadingPackage.java

--- android/src/main/java/com/reactlibrary/compassheading/CompassHeadingPackage.java
+++ android/src/main/java/com/reactlibrary/compassheading/CompassHeadingPackage.java
@@ -16,11 +16,6 @@ public class CompassHeadingPackage implements ReactPackage {
     }
 
     @Override
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-        return null;
-    }
-
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
