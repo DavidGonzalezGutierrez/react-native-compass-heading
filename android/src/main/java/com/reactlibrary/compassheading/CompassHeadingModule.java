7247d9f73c03a8c18a0a4001bf37c82f7ead10c2
David GOonzález <benaventebiker_007@hotmail.com>
2021-06-22 11:39:20 +0200

    Compensating roll and picht in android

--- android/src/main/java/com/reactlibrary/compassheading/CompassHeadingModule.java
+++ android/src/main/java/com/reactlibrary/compassheading/CompassHeadingModule.java
@@ -100,6 +100,7 @@ public class CompassHeadingModule extends ReactContextBaseJavaModule implements 
     @Override
     public void onSensorChanged(SensorEvent event) {
         float[] mMatrixR = new float[9];
+        float[] outRotationMatrix;
         float[] mMatrixValues = new float[3];
         final float alpha = 0.97f;
 
@@ -107,9 +108,15 @@ public class CompassHeadingModule extends ReactContextBaseJavaModule implements 
             if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
                 SensorManager.getRotationMatrixFromVector(mMatrixR, event.values);
                 SensorManager.getOrientation(mMatrixR, mMatrixValues);
+
+                int roll = (int) Math.toDegrees(mMatrixValues[2]);
+
+                outRotationMatrix = remapCoordinatesSystem(mMatrixR, roll);
+                SensorManager.getOrientation(outRotationMatrix, mMatrixValues);
+
                 int newAzimuth = (int) Math.toDegrees(mMatrixValues[0]);
-                newAzimuth = displayRotation((newAzimuth + 360) % 360);
-                emitResponse(newAzimuth);
+
+                emitResponse((newAzimuth + 360) % 360);
             } else {
                 if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
 
@@ -145,6 +152,35 @@ public class CompassHeadingModule extends ReactContextBaseJavaModule implements 
                 }
             }
         }
+    }
+
+   public float[] remapCoordinatesSystem (float[] rotationMatrix, int roll) {
+        Display disp = (((WindowManager) mApplicationContext.getSystemService(Context.WINDOW_SERVICE))).getDefaultDisplay();
+        float[] outRotationMatrix = new float[9];
+        int rotation = disp.getRotation();
+
+        System.out.println("roooll" + roll);
+
+        if(roll != 0){
+            SensorManager.remapCoordinateSystem(rotationMatrix, SensorManager.AXIS_X, SensorManager.AXIS_Z, outRotationMatrix);
+        } else {
+            switch (rotation) {
+                case Surface.ROTATION_0:
+                    SensorManager.remapCoordinateSystem(rotationMatrix, SensorManager.AXIS_X, SensorManager.AXIS_Y, outRotationMatrix);
+                    break;
+                case Surface.ROTATION_90:
+                    SensorManager.remapCoordinateSystem(rotationMatrix, SensorManager.AXIS_Y, SensorManager.AXIS_MINUS_X, outRotationMatrix);
+                    break;
+                case Surface.ROTATION_180:
+                    SensorManager.remapCoordinateSystem(rotationMatrix, SensorManager.AXIS_MINUS_X, SensorManager.AXIS_MINUS_Y, outRotationMatrix);
+                    break;
+                case Surface.ROTATION_270:
+                    SensorManager.remapCoordinateSystem(rotationMatrix, SensorManager.AXIS_MINUS_Y, SensorManager.AXIS_X, outRotationMatrix);
+                    break;
+            }
+        }
+
+       return outRotationMatrix;
     }
 
     public int displayRotation (int azimuth) {
